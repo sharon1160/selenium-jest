c46043b6990c5dd008036df255489ea72178d6e7
sharon1160 <schullunquia@unsa.edu.pe>
2020-11-18 10:53:05 -0500

    Agregando driver en las funciones de utilidad

--- pruebas-funcionales/utilidades.js
+++ pruebas-funcionales/utilidades.js
@@ -3,13 +3,13 @@ const { By, until } = require('selenium-webdriver')
 const waitUntilTime = 20000
 
 // funciones de utilidad
-async function getElementById(id) {  
+async function getElementById(id, driver) {  
     const el = await driver.wait(until.elementLocated(By.id(id)), waitUntilTime)  
 
     return await driver.wait(until.elementIsVisible(el), waitUntilTime
 )}
 
-async function getElementByXPath(xpath) {  
+async function getElementByXPath(xpath, driver) {  
     const el = await driver.wait(until.elementLocated(By.xpath(xpath)), waitUntilTime)  
     return await driver.wait(until.elementIsVisible(el), waitUntilTime
 )}
